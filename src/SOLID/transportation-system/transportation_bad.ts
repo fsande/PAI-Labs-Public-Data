35f76da57e869333262400a044829fa07659e02a
fsande <fsande@ull.es>
2025-03-18 11:02:09 +0000

    Transportation System

---
+++ src/SOLID/transportation-system/transportation_bad.ts
@@ -0,0 +1,45 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas 2021-2022
+ *
+ * @author F. de Sande
+ * @since Feb 27 2024
+ * @description El programa modela un sistema de gestión de medios de transporte
+ *              El diseño viola uno de los principios SOLID
+ */
+
+class Transportation {
+  constructor(private transporter: string, private volume: number) {
+	  this.transporter = transporter;
+	  this.volume = volume;
+  }
+
+  /**
+   * @description Computes the price of the transportation
+   * @returns The price of the transportation
+   */
+  computePrice(): number {
+    switch (this.transporter) {
+      case 'Truck':
+  	    return (500 * this.volume);  
+        break;
+      case 'Ship':
+  	    return (300 * this.volume);  
+        break;
+      default:
+        throw new Error('Unsupported Transport media');
+        break;
+    }
+  }
+}
+
+export function main(): void { 
+  const truck = new Transportation('Truck', 100);
+  const ship = new Transportation('Ship', 5000);
+  console.log('Trasnport price for truck: ' + truck.computePrice()); 
+  console.log('Trasnport price for ship: ' + ship.computePrice()); 
+}
+
+main();
---
+++ src/SOLID/transportation-system/transportation_good.ts
@@ -0,0 +1,21 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas 2021-2022
+ *
+ * @author F. de Sande
+ * @since Feb 27 2024
+ * @description El programa modela un sistema de gestión de medios de transporte
+ *              El diseño violaba el principio SOLID XXX debido a que ...  
+ *              Este nuevo diseño no viola el principio
+ */
+
+export function main(): void { 
+  // const truck = new Transportation('Truck', 100);
+  // const ship = new Transportation('Ship', 5000);
+  // console.log('Trasnport price for truck: ' + truck.computePrice()); 
+  // console.log('Trasnport price for ship: ' + ship.computePrice()); 
+}
+
+main();
