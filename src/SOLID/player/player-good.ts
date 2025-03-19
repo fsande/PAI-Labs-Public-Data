6a5d0427efbda8f68d1fa6d3f99278981b5dc7ca
fsande <fsande@ull.es>
2025-03-19 10:56:05 +0000

    player system example

---
+++ src/SOLID/player/player-good.ts
@@ -0,0 +1,37 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author YOUR NAME
+ * @since Mar 19, 2025
+ * @description Program to manage different kinds of Media Players
+ *
+ *              EXPLAIN (English or Spanish) the reasons why the original code violates some SOLID Principle
+ * 
+ */
+
+export function main(): void { 
+  /*
+ 
+     Your main function should be very similar to the original:
+
+  const cassette = new Cassette();
+  console.log(cassette.playAudio());
+  console.log(cassette.recordAudio());
+  console.log(cassette.playVideo());
+
+  const tv = new Tv();
+  console.log(tv.playAudio());
+  console.log(tv.recordAudio());
+  console.log(tv.playVideo());
+
+  const playStation = new VideoGameConsole();
+  console.log(playStation.playAudio());
+  console.log(playStation.recordAudio());
+  console.log(playStation.playVideo());
+  */
+}
+
+main();
